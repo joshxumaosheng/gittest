d624b2f5b579fdcf60e620dddaeb923257e73442
Admin <946421519@qq.com>
2019-08-10 17:16:42 +0800

    本地仓库

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,27 @@
+package com.itheima.pojo;
+
+public class User {
+    String username;
+
+    public User() {
+    }
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                '}';
+    }
+}
